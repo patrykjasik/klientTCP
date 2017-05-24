64a1f5fc32b34c25968faaf618bc2f53eea1aa64
student <student@NANO-01>
2017-05-24 09:40:52 +0200

    Powiększono okno i dodano przycisk zamknij

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.przycisk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // przycisk
+            // 
+            this.przycisk.Location = new System.Drawing.Point(291, 420);
+            this.przycisk.Name = "przycisk";
+            this.przycisk.Size = new System.Drawing.Size(257, 70);
+            this.przycisk.TabIndex = 0;
+            this.przycisk.Text = "Zamknij";
+            this.przycisk.UseVisualStyleBackColor = true;
+            this.przycisk.Click += new System.EventHandler(this.przycisk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 502);
+            this.Controls.Add(this.przycisk);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button przycisk;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace klientTCP
         {
             InitializeComponent();
         }
+
+        private void przycisk_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
